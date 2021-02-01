d6fae8e03f2d5996cb241a49c77656066cb95565
LHBThien <yunheemi.troi@gmail.com>
2021-02-01 11:09:19 +0700

    update content modules

--- wp-content/themes/tatoo/header.php
+++ wp-content/themes/tatoo/header.php
@@ -44,4 +44,4 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
 <body>
 
-	<?php echo get_template_part( 'template-parts/modules/tatoo-modules/modules-module_banner' ); ?>
+	<?php echo get_template_part( 'template-parts/modules/modules_home/module-module_banner' ); ?>
--- wp-content/themes/tatoo/page.php
+++ wp-content/themes/tatoo/page.php
@@ -28,9 +28,9 @@ get_header();
       
 <?php	
 if ( have_posts() ) : the_post();		
-    if ( have_rows('tatoo-modules') ) :
-      while ( have_rows('tatoo-modules') ) : the_row();
-        echo get_template_part('template-parts/modules/tatoo-modules/modules', get_row_layout());
+    if ( have_rows('modules_home') ) :
+      while ( have_rows('modules_home') ) : the_row();
+        echo get_template_part('template-parts/modules/modules_home/module', get_row_layout());
       endwhile;
   endif; 
 endif;
